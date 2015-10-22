06712a8ef708a3364f3e1948d863de1afc5140f3
HanHangit <clonkcool@yahoo.de>
2015-10-22 13:35:10 +0200

    Ein kleiner Test zum Testen.

---
+++ Test7/Test7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Test01");
+            window.Closed += (object sender, EventArgs e) => { (sender as Window).Close(); };
+            while (window.IsOpen())
+            {
+                window.Clear();
+                window.Display();
+                window.DispatchEvents();
+            }
+        }
+    }
+}
